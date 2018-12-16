bd7a8c6cd3b9bbc356d4cb7dbf94da02f1e8ff18
Romain Tholimet <forstulen@romains-macbook-pro-1.home>
2018-12-16 13:32:29 +0100

    [Audio-Visualizer]Initial Commit

---
+++ Assets/Scripts/AudioVisualizerScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioVisualizerScript : MonoBehaviour {
+
+    public LineRendererDataScript[] RendererData;
+    public Text                     NameSongLabel;
+    public Text                     TimeLabel;
+
+    private const int QSamples = 1024;
+
+    float[] _samples;
+    float[] _freqBand;
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        //Initialize buffers
+        _samples    = new float[QSamples];
+        _freqBand    = new float[RendererData.Length];
+        _audioSource = GetComponent<AudioSource>();
+        NameSongLabel.text = _audioSource.clip.name;
+    }
+
+    void Update()
+    {
+        TimeLabel.text = _audioSource.time.ToString();
+
+        //Get the audio data
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.BlackmanHarris);
+
+        int count = 0;
+
+        //Calculate an average value for a frequency range
+        for (var i = 0; i < RendererData.Length; ++i)
+        {
+            float average = 0;
+            int sampleCount = _samples.Length / RendererData.Length;
+
+            for (var j = sampleCount * i; j < sampleCount * i + sampleCount; ++j) {
+                if (count < _samples.Length)
+                    average += _samples[count] * (count + 1);
+                ++count;
+            }
+
+            average /= sampleCount;
+
+            _freqBand[i] = average;
+
+            //Add a new value each frame to renderer script
+            RendererData[i].AddPoint(_freqBand[i]);
+        }
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/LineRendererDataScript.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineRendererDataScript : MonoBehaviour {
+
+    public int NumberOfPoints = 40;
+
+    private float[] _points;
+
+    private int     _currentIndex;
+
+    private LineRenderer _renderer;
+
+	// Use this for initialization
+	void Start () {
+        //Initialize buffers
+        _points = new float[NumberOfPoints];
+        //Set the current index to add a new point into the renderer line
+        _currentIndex = (int)(NumberOfPoints / 1.3);
+        _renderer = GetComponent<LineRenderer>();
+
+        //Set the number of points for the renderer line
+        _renderer.positionCount = NumberOfPoints;
+	}
+	
+    public void AddPoint(float value) {
+        //Create a copy of the old array to shift values to the left
+        var newArray = new float[NumberOfPoints];
+
+        Array.Copy(_points, 1, newArray, 0, _currentIndex);
+        //****  FASTEST WAY TO ACHIEVE THIS : https://stackoverflow.com/questions/2381245/c-sharp-quickest-way-to-shift-array
+
+        _points = newArray;
+        _points[_currentIndex] = Mathf.Clamp(value, -2, 2);
+
+        for (var i = 0; i < NumberOfPoints; ++i) {
+            var z       = -_points[i] * GetCoeff(i);
+
+            //Add a fake value after the current index to decrease the "gap effect" at the current index
+            if (i > _currentIndex) {
+                z = (-_points[_currentIndex] * GetCoeff(_currentIndex)) / (float)Math.Pow((i - _currentIndex), 1.2);
+            }
+            //Set a vector for the new point
+            Vector3 vector = new Vector3(Mathf.Abs(i * (_renderer.transform.position.x * 2.0f) / NumberOfPoints), 0, z);
+
+            _renderer.SetPosition(i, vector);
+        }
+    }
+
+    private float GetCoeff(int index) {
+
+        //var value = 1.0f;
+
+        //Return a value from a function to create a curve : https://www.wolframalpha.com/input/?i=interpolate+%5B(0,+0.05),+(20,+0.05)+(40,+1),+(50,+1)+,+(60,+1),+(80,+0.05),+(100,+0.05)%5D
+        var value = 7.25694 * Mathf.Pow(10, -10) * Mathf.Pow(index, 6) - 2.17708 * Mathf.Pow(10, -7) * Mathf.Pow(index, 5) + 0.0000251684 * Mathf.Pow(index, 4) - 0.00140521 * Mathf.Pow(index, 3) + 0.037554 * Mathf.Pow(index, 2) - 0.357833 * index + 0.05;
+
+        return Mathf.Clamp((float)value, 0.05f, 1.0f);
+
+    }
+}
